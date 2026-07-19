eaa2db69c47dbf920d5724885178fb317bc4f8e7
124Ashish <guihgkiu@gmail.com>
2026-07-19 23:03:32 +0530

    Fix nodemailer smtp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -156,7 +156,9 @@ exports.sendOTP = async(req, res) => {
         otpStore[email] = otp;
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
